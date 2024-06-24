0f477dbf1f8081f0b15a58543fecb92a6c64dd29
lien teng <olienteng895@gmail.com>
2024-06-24 16:46:20 +0700

    clear debug on console.log

--- src/app/pages/my-cloud/MyCloud.tsx
+++ src/app/pages/my-cloud/MyCloud.tsx
@@ -958,7 +958,6 @@ export function MyCloud() {
     setTimeout(() => {
       setOptionsValue(false);
     }, 500);
-    console.log(dataForEvent.data)
 
     const checkPassword = isCheckPassword();
 
