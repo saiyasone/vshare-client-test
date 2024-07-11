277210e50c3eab976c584c3b74dd4142acbd67f0
lien teng <olienteng895@gmail.com>
2024-07-11 16:32:34 +0700

    update payment on 2 checkout

--- src/app/pages/price/sections/payment/TwoPaymentCheckout.tsx
+++ src/app/pages/price/sections/payment/TwoPaymentCheckout.tsx
@@ -1,8 +1,15 @@
 import { Fragment } from "react";
-import { Box } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
+import { Box, Button, styled } from "@mui/material";
+import PaymentIcon from "assets/images/wallet-checkout.png";
+import IconAction from "@mui/icons-material/Payment";
 
 import { ENV_KEYS } from "constants/env.constant";
+
+const ImageIcon = styled("img")({
+  width: "150px",
+  height: "150px",
+  objectFit: "cover",
+});
 
 type Prop = {
   packageId: string;
@@ -18,10 +25,28 @@ function TwoPaymentCheckout({ packageId }: Prop) {
   };
   return (
     <Fragment>
-      <Box>
-        <LoadingButton variant="contained" onClick={handleTwoPaymentCheckout}>
-          Payment
-        </LoadingButton>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ImageIcon src={PaymentIcon} alt="payment-icons" />
+
+          <Box sx={{ marginTop: "2rem" }}>
+            <Button variant="contained" onClick={handleTwoPaymentCheckout}>
+              <IconAction sx={{ mr: 2 }} /> Payments
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Fragment>
   );
