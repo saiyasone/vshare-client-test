d7b2ce397c037c0b84a9b67c426be06e6eb1703b
lien teng <olienteng895@gmail.com>
2024-08-16 14:13:07 +0700

    update something

--- src/hooks/useResizeImage.tsx
+++ src/hooks/useResizeImage.tsx
@@ -2,7 +2,18 @@ import axios from "axios";
 import { ENV_KEYS } from "constants/env.constant";
 import _ from "lodash";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { encryptData } from "utils/secure.util";
+const { CancelToken } = axios;
+
+type Props = {
+  imagePath: string;
+  user: any;
+  fileType: string;
+  width: number | string;
+  height: number | string;
+  isPublic: boolean;
+};
 
 const useResizeImage = ({
   imagePath,
@@ -10,17 +21,20 @@ const useResizeImage = ({
   fileType,
   width = 200,
   height = 200,
-  storageZoneName = ENV_KEYS.VITE_APP_STORAGE_ZONE,
   isPublic = false,
-}) => {
+}: Props) => {
   const [imageSrc, setImageSrc] = useState<any>("");
   const [imageFound, setImageFound] = useState<any>(null);
+  const location = useLocation();
+
+  const source = CancelToken.source();
+  const cancelToken = source.token;
+
   useEffect(() => {
     if (fileType === "image") {
       const fetchResizeImage = async (imagePath, userId) => {
         try {
           const enData = encryptData({
-            storageZoneName,
             path: imagePath,
             createdBy: isPublic ? "0" : userId,
             width: `${width}`,
@@ -31,6 +45,7 @@ const useResizeImage = ({
             `${ENV_KEYS.VITE_APP_LOAD_URL}downloader/file/resize-image?file=${enData}`,
             {
               responseType: "arraybuffer",
+              cancelToken,
             },
           );
           if (_.isArrayBuffer(res.data)) {
@@ -49,6 +64,9 @@ const useResizeImage = ({
             setImageFound(false);
           }
         } catch (e) {
+          if (axios.isCancel(e)) {
+            console.log("Request canceled", e.message);
+          }
           setImageFound(false);
         }
       };
@@ -57,8 +75,12 @@ const useResizeImage = ({
       } else {
         setImageFound(false);
       }
+
+      return () => {
+        source.cancel("Operation canceled due to route change.");
+      };
     }
-  }, [imagePath, user, fileType]);
+  }, [imagePath, user, fileType, location]);
 
   return { imageSrc, imageFound };
 };
